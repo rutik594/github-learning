9fc47f5110ad3a57306086d20f0bf43a6d46c8b9
rutik594 <rutikbhanushali25@gmail.com>
2021-08-17 00:57:56 +0530

    phase1

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app =express()
 const PORT = 3002
 app.listen(PORT, (req, res) => {
-    res.send('Connected to server')
+    res.send('Connected to server1')
 })
